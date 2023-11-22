8b74e073bd3b67eb76260ffb81fb3764bdb32d4c
abhiSolankii <abhisheksolanki01010@gmail.com>
2023-11-23 02:33:31 +0530

    digital-clock

--- digitalclock.js
+++ digitalclock.js
@@ -6,17 +6,8 @@ function display() {
   document.getElementById("hours").innerHTML = hours;
   document.getElementById("minutes").innerHTML = minutes;
   document.getElementById("seconds").innerHTML = seconds;
-  // document.getElementById("dots-1").innerHTML = " ";
-  // document.getElementById("dots-2").innerHTML = " ";
 }
 setInterval(display, 1000);
-
-// setInterval(() => {
-//   document.getElementById("dots-1").innerHTML = ":";
-// }, 100);
-// setInterval(() => {
-//   document.getElementById("dots-2").innerHTML = ":";
-// }, 100);
 
 function visibleDots() {
   const dd = new Date();
